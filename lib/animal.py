6ef65889081b56e0a2ee4f626067aaeb03b7c30c
Matteo Piccini <gnappo.1@live.it>
2024-10-31 14:05:19 -0700

    75% done with lab

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,54 @@
 class Animal:
-    pass
+    all_animals = []  #! class attr
+
+    def __init__(self, nickname, species, weight, zoo):
+        self.nickname = nickname  #! instance attr
+        self.species = species  #! instance attr
+        self.weight = weight  #! instance attr
+        self.zoo = zoo  #! instance attr
+        type(self).all_animals.append(self)
+
+    @property
+    def nickname(self):
+        return self._nickname
+
+    @nickname.setter
+    def nickname(self, nickname_str):
+        if not isinstance(nickname_str, str):
+            raise TypeError("Nickname must be a string")
+        self._nickname = nickname_str
+
+    @property
+    def species(self):
+        return self._species
+
+    @species.setter
+    def species(self, species_str):
+        if not isinstance(species_str, str):
+            raise TypeError("Species must be a string")
+        elif species_str not in ( "Cat", "Dog", "Rat"):
+            raise ValueError("Species must be one of  Cat, Dog, Rat")
+        self._species = species_str
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight_str):
+        if not isinstance(weight_str, float):
+            raise TypeError("Weight must be a string")
+        self._weight = weight_str
+
+    @property
+    def zoo(self):
+        return self._zoo
+
+    @zoo.setter
+    def zoo(self, zoo_instance):
+        from zoo import Zoo
+        if not isinstance(zoo_instance, Zoo):
+            raise TypeError("Zoo must be of type Zoo")
+        self._zoo = zoo_instance
+
+import ipdb; ipdb.set_trace()
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,35 @@
-from lib.animal import Animal
+from animal import Animal
 
+# Zoo() -> __create__ -> __init__
 class Zoo:
-    pass
+    all_zoos = [] #! class attr
+
+    def __init__(self, name, location):
+        self.name = name #! instance attr
+        self.location = location  #! instance attr
+        type(self).all_zoos.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name_str):
+        if not isinstance(name_str, str):
+            raise TypeError("Name must be a string")
+        self._name = name_str
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, location_str):
+        if not isinstance(location_str, str):
+            raise TypeError("Location must be a string")
+        self._location = location_str
+
+    def animals(self):
+        return [animal for animal in Animal.all_animals if animal.zoo is self]
+
+import ipdb; ipdb.set_trace()
